33fdaab07fa105d7c132f4b07bb6b955f5afb88a
iPenguwin <220711663@students.uajy.ac.id>
2025-06-30 14:43:28 +0700

    masih coba
    masih coba

---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
